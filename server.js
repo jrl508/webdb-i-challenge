4604c19e92c2d8abf8723e716d1acb8016df9801
jrl508 <jrl47@yahoo.com>
2019-07-15 22:05:17 -0400

    Most of mvp completed, need to complete update and delete functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const accounts = require('./accounts/accountsRouter')
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/accounts', accounts)
 
 module.exports = server;
